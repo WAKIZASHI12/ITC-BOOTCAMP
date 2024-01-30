e0b6310b907fe867da73def94e08f23f589d97fd
wakizashi <wakizashi.terehel@gmail.com>
2024-01-30 14:20:50 +0600

    problem58910 x.py

---
+++ x.py
@@ -0,0 +1,33 @@
+result =  -21//10
+print(result)
+
+
+
+
+birth_year = 2004
+current_year = 2024
+
+age_in_future = current_year - birth_year + 2
+age_two_years_ago = current_year - birth_year - 2
+
+if age_in_future > age_two_years_ago:
+    print("Через 2 года вам будет больше, чем два года назад.")
+elif age_in_future < age_two_years_ago:
+    print("Через 2 года вам будет меньше, чем два года назад.")
+else:
+    print("Через 2 года вам будет столько же, сколько два года назад.")
+
+a = 19
+b = 100
+print(a/b)
+
+
+a = 25
+b = 75
+d = 10
+c = 95
+resu = (a + b + d + c) / 4
+print("srednie:",resu)
+
+
+
