90ffd33258acc079b6b60a481236869f018d9f4f
wakizashi <wakizashi.terehel@gmail.com>
2024-01-30 13:56:04 +0600

    trttt c.py

---
+++ c.py
@@ -0,0 +1,49 @@
+a = (17*3)
+b = (12*5)
+c = (12**3)
+d = (13*7)
+e = (4**5)
+f = (512+512)
+print(a < b)
+print(c > d)
+print(e == f)
+
+
+
+
+
+result1 = 34 ** 2
+result2 = 26 * 3
+result3 = 17 * 33
+result4 = 4394 * 4
+
+print("Результат для (34**2):", result1, ">= 17925 -", result1 >= 17925)
+print("Результат для (26*3):", result2, ">= 17925 -", result2 >= 17925)
+print("Результат для (17*33):", result3, ">= 17925 -", result3 >= 17925)
+print("Результат для (4394*4):", result4, ">= 17925 -", result4 >= 17925)
+
+
+a = 46
+b = 898
+c = 1834
+resy = ((a+b) * c)** 2 - 193432
+print(resy)
+
+a = 4.8
+b = 3
+c = 7
+
+result = a > b and a < c
+result2 = (7 % 3) * 4.8
+print("result:",a > b and a < c)
+print("result2:",(7 % 3) * 4.8)
+
+exi = 7 * 3
+exi2 = 21
+print("exi:",7*3)
+print("exi2:",21)
+
+exi3 = 7 + 3
+exi4 = 10
+print("exi3:",7+3)
+print("exi3:",10)
