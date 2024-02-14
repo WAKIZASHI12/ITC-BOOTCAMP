e9631b430db2d2ca330eb3c994978b4c79c84b34
wakizashi <wakizashi.terehel@gmail.com>
2024-02-14 12:18:32 +0600

    delete

--- work/cisla.py
+++
@@ -1,9 +0,0 @@
-result = []
-for i in range(1000):
-    if i % 3 == 0 or  i % 5 == 0:
-         result.append(i)
-
-counter = 0
-for x in result:
-    counter = counter + x
-print(counter)
--- work/cisla2.py
+++
@@ -1,5 +0,0 @@
-sring = "4729461084" 
-result =  0
-for i in sring:
-    result += int(i)
-print(result)
